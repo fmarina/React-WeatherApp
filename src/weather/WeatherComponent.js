f5bc61be123e7bfed67797953b3437a09d7bfd06
Marina <marinafaenze@hotmail.com>
2020-06-23 22:13:36 -0300

    fetching openweather data

--- src/weather/WeatherComponent.js
+++ src/weather/WeatherComponent.js
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 import cloudy from '../assets/images/cloudy.png';
 import sunny from '../assets/images/sunny.png';
 import rainy from '../assets/images/rainy.png';
 
+const api = {
+    key: "2461ad88218bfad4cb98068a09c702e8",
+    url: "api.openweathermap.org/data/2.5/weather?q="
+}
+
 const Weather = (props) => {
+
+    const [value, setValue] = useState("");
+    const [weather, setWeather] = useState({});
+
+    const handleKeyPress = (event) => {
+        if(event.key === "Enter") {
+
+            fetch(`http://${api.url}${value}&appid=${api.key}`)
+            .then(response => response.json())
+            .then(result => {
+                console.log(result);
+                setWeather(result);
+                setValue("");
+            });
+        }
+    }
+    
     return (
         <div className="form-container">
             <input
                 type="text"
+                value={value}
                 placeholder="Enter the city" 
-                autoFocus               
+                autoFocus
+                onChange={event => setValue(event.target.value)}
+                onKeyPress={handleKeyPress}
             />
+
             <div className="card-weather">
                 <label>Monday, 27th April</label>
                 <label>6:27am</label>
